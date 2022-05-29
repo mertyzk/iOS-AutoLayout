690fc3e6398f5ffadc7d2263b76d2d9e4c788762
RinKusChi <meyuzak@gmail.com>
2022-05-29 20:23:29 +0300

    29 May 2022
    Top Spacing - Leading Spacing - Trailing Spacing - Bottom Spacing

---
+++ AutoLayoutStoryBoard/AutoLayoutStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutStoryBoard
+//
+//  Created by Macbook Air on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

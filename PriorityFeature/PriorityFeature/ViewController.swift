073bf07d131616b784f46ba00adda11bc8695c7c
RinKusChi <meyuzak@gmail.com>
2022-05-29 22:54:25 +0300

    29 May 2022
    Priorty Feature

---
+++ PriorityFeature/PriorityFeature/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PriorityFeature
+//
+//  Created by Macbook Air on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnClickComment(_ sender: UIButton) {
+        sender.removeFromSuperview()
+    }
+
+    
+}
+
